0e9146a9ce693da99d5d55c1049e23ffc428ff7d
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-03-18 14:50:32 +0100

    OnboardingViewModel hinzugefügt

--- app/src/main/java/com/example/adventurebook/data/viewmodel/OnboardingViewModel.kt
+++ app/src/main/java/com/example/adventurebook/data/viewmodel/OnboardingViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.adventurebook.data.viewmodel
 
-class OnboardingViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.adventurebook.data.local.Avatar
+import com.example.adventurebook.data.repos.AvatarRepoInterface
+
+class OnboardingViewModel(private val avatarRepo: AvatarRepoInterface): ViewModel() {
+    suspend fun saveAvatar(name: String, gender: String, age: String) {
+        val avatar = Avatar(
+            name = name,
+            gender = gender,
+            age = age
+        )
+
+        avatarRepo.insertAvatar(avatar)
+    }
+
+    suspend fun getAvatar(): Avatar? = avatarRepo.getAvatar()
 }
