265fb8592005ee6723486dcbcdfa6c22a7512aeb
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-03-19 19:58:11 +0100

    LibraryScreen Demo hinzugefügt

--- app/src/main/java/com/example/adventurebook/ui/ui/screens/LibraryScreen.kt
+++ app/src/main/java/com/example/adventurebook/ui/ui/screens/LibraryScreen.kt
@@ -1,2 +1,62 @@
 package com.example.adventurebook.ui.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import com.example.adventurebook.data.viewmodel.StoryViewModel
+
+@Composable
+fun LibraryScreen(
+    navController: NavController,
+    viewModel: StoryViewModel
+) {
+    val stories by viewModel.allStories.collectAsState(initial = emptyList())
+
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
+        items(stories) { story ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+                    .clickable { navController.navigate("story/${story.id}") },
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(modifier = Modifier.padding(16.dp)) {
+                    AsyncImage(
+                        model = story.ImageUrl,
+                        contentDescription = "Story Image",
+                        modifier = Modifier.size(50.dp),
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Column {
+                        Text(story.title, style = MaterialTheme.typography.headlineMedium)
+                        Text(story.content.take(50) + "...", style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
+            }
+        }
+    }
+}
