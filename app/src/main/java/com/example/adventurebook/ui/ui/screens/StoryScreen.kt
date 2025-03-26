18f5338d0427f2a67bc75e62195b84eff3afe0ce
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-03-26 13:41:12 +0100

    StoryScreen updated

--- app/src/main/java/com/example/adventurebook/ui/ui/screens/StoryScreen.kt
+++ app/src/main/java/com/example/adventurebook/ui/ui/screens/StoryScreen.kt
@@ -14,13 +14,27 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,6 +46,7 @@ import androidx.navigation.NavController
 import coil3.compose.AsyncImage
 import com.example.adventurebook.data.viewmodel.StoryViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StoryScreen(
     navhController: NavController,
@@ -39,6 +54,7 @@ fun StoryScreen(
 ) {
     val story by viewModel.currentStory.collectAsState()
     val context = LocalContext.current
+    var currentParagraph by remember { mutableIntStateOf(0) }
 
     Box(modifier = Modifier.fillMaxSize()) {
         if (story == null) {
@@ -48,6 +64,7 @@ fun StoryScreen(
                     .size(50.dp)
             )
         } else {
+            val paragraphs = story!!.content.split("\n\n").filter { it.isNotBlank() }
             AsyncImage(
                 model = story!!.ImageUrl,
                 contentDescription = "Story Image",
@@ -55,35 +72,88 @@ fun StoryScreen(
                 contentScale = ContentScale.Crop
             )
 
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth()
-                    .heightIn(max = 300.dp)
-                    .background(Color.Black.copy(alpha = 0.7f))
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.SpaceBetween
-
-            ) {
-                Text(story!!.title, style = MaterialTheme.typography.headlineMedium, color = Color.White)
-                Text(story!!.content, style = MaterialTheme.typography.bodyMedium, color = Color.White)
-
-                Row {
-                    Button(onClick = {
-                        viewModel.saveCurrentStory { id ->
-                            Toast.makeText(context, "Geschichte gespeichert", Toast.LENGTH_SHORT).show()
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(story!!.title) },
+                        actions = {
+                            IconButton(onClick = {
+                                viewModel.saveCurrentStory { id ->
+                                    Toast.makeText(context, "Geschichte gespeichert", Toast.LENGTH_SHORT).show()
+                                }
+                            }) {
+                                Icon(Icons.Default.Favorite, contentDescription = "Save")
+                            }
                         }
-                    }) {
-                        Text("Speichern")
-                    }
+                    )
+                }
+            ) { padding ->
+                Column(
+                    modifier = Modifier
+                        .padding(padding)
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        //.heightIn(max = 300.dp)
+                        .background(Color.Black.copy(alpha = 0.7f))
+                        .padding(16.dp),
+                    //verticalArrangement = Arrangement.SpaceBetween
+                    horizontalAlignment = Alignment.CenterHorizontally
 
-                    Spacer(modifier = Modifier.width(8.dp))
+                ) {
+//                    Text(story!!.title, style = MaterialTheme.typography.headlineMedium, color = Color.White)
+//                    Text(story!!.content, style = MaterialTheme.typography.bodyMedium, color = Color.White)
+//
+//                    Row {
+//                        Button(onClick = {
+//                            viewModel.saveCurrentStory { id ->
+//                                Toast.makeText(context, "Geschichte gespeichert", Toast.LENGTH_SHORT).show()
+//                            }
+//                        }) {
+//                            Text("Speichern")
+//                        }
+//
+//                        Spacer(modifier = Modifier.width(8.dp))
+//
+//                        Button(onClick = {
+//                            // Fortsetzen später integrieren
+//                            Toast.makeText(context, "Fortsetzung noh nicht implementiert", Toast.LENGTH_SHORT).show()
+//                        }) {
+//                            Text("Fortsetzen")
+//                        }
+//                    }
+                    Text(
+                        text = paragraphs.getOrNull(currentParagraph) ?: "Wie soll es weitergehen?",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.White
+                    )
 
-                    Button(onClick = {
-                        // Fortsetzen später integrieren
-                        Toast.makeText(context, "Fortsetzung noh nicht implementiert", Toast.LENGTH_SHORT).show()
-                    }) {
-                        Text("Fortsetzen")
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(
+                            onClick = { if (currentParagraph > 0) currentParagraph-- },
+                            enabled = currentParagraph > 0
+                        ) {
+                            Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = "Back")
+                        }
+
+                        if (currentParagraph == paragraphs.size - 1) {
+                            Button(
+                                onClick = { /* Interaktionsfeature*/ }
+                            ) {
+                                Text("Fortsetzen")
+                            }
+                        }
+
+                        IconButton(
+                            onClick = { if (currentParagraph < paragraphs.size - 1) currentParagraph++ },
+                            enabled = currentParagraph < paragraphs.size - 1
+                        ) {
+                            Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = "Next")
+                        }
                     }
                 }
             }
