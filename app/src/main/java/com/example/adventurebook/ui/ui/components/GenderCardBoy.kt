5814ce430f96fc442f62a367c5c4bcb8d1eb120a
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-03-27 14:17:51 +0100

    OnBoardingScreen überarbeitet

---
+++ app/src/main/java/com/example/adventurebook/ui/ui/components/GenderCardBoy.kt
@@ -0,0 +1,63 @@
+package com.example.adventurebook.ui.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.adventurebook.R
+
+@Composable
+fun GenderCardBoy(isSelected: Boolean, onClick: () -> Unit) {
+    OutlinedCard(
+        modifier = Modifier
+            .size(width = 130.dp, height = 180.dp)
+            .border(if (isSelected) 2.dp else 2.dp, if (isSelected) Color.Blue else Color.Gray, shape = RoundedCornerShape(12.dp))
+            .clickable { onClick() },
+        shape = CardDefaults.outlinedShape,
+        elevation = CardDefaults.cardElevation(if (isSelected) 6.dp else 0.dp),
+        colors = CardColors(
+            containerColor = Color.DarkGray,
+            contentColor = Color.White,
+            disabledContainerColor = Color.Gray,
+            disabledContentColor = Color.Gray,
+        )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Junge")
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "Boy"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun GenderCardPrev() {
+    GenderCardBoy(
+        isSelected = true
+    ) { }
+}
--- app/src/main/java/com/example/adventurebook/ui/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/example/adventurebook/ui/ui/screens/OnboardingScreen.kt
@@ -2,8 +2,11 @@ package com.example.adventurebook.ui.ui.screens
 
 import android.app.Activity
 import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,12 +14,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,13 +32,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.adventurebook.MainActivity
+import com.example.adventurebook.data.repos.AvatarRepoInterface
 import com.example.adventurebook.data.viewmodel.OnboardingViewModel
+import com.example.adventurebook.ui.ui.components.GenderCardBoy
+import com.example.adventurebook.ui.ui.components.GenderCardGirl
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun OnBoardingScreen(viewModel: OnboardingViewModel) {
@@ -39,72 +53,89 @@ fun OnBoardingScreen(viewModel: OnboardingViewModel) {
     var ageRange by remember { mutableStateOf("") }
     var context = LocalContext.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.DarkGray)
     ) {
-        Text("Profil erstellen")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Avatar erstellen", color = Color.White, style = MaterialTheme.typography.headlineSmall)
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
-        Row {
-            Button(onClick = { gender = "Junge"} ) { Text("Junge") }
-            Spacer(modifier = Modifier.width(8.dp))
-            Button(onClick = { gender = "Mädchen"} ) { Text("Mädchen") }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = name,
-            onValueChange = { name = it },
-            placeholder = { Text("Name eingeben") }
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text("Alter auswählen", style = MaterialTheme.typography.bodyMedium)
-
-        Row {
-            Card(
-                modifier = Modifier
-                    .padding(4.dp)
-                    .clickable { ageRange = "6-8" },
-                elevation = CardDefaults.cardElevation( if (ageRange == "6-8") 8.dp else 2.dp)
-            ) {
-                Text("6-8", modifier = Modifier.padding(8.dp))
+            Row {
+                GenderCardGirl(onClick = { gender = "Mädchen"}, isSelected = gender.contains("Mädchen") )
+                Spacer(modifier = Modifier.width(16.dp))
+                GenderCardBoy(onClick = { gender = "Junge"}, isSelected = gender.contains("Junge"))
             }
 
-            Card(
-                modifier = Modifier
-                    .padding(4.dp)
-                    .clickable { ageRange = "8-10" },
-                elevation = CardDefaults.cardElevation( if (ageRange == "8-10") 8.dp else 2.dp)
-            ) {
-                Text("8-10", modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                placeholder = { Text("Name eingeben") },
+                label = { Text("Name") },
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text("Alter auswählen", color = Color.White, style = MaterialTheme.typography.bodyMedium)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row {
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clickable { ageRange = "6-8" }
+                        .border(width = if (ageRange == "6-8") 2.dp else 2.dp, color = if (ageRange == "6-8") Color.White else Color.Gray, shape = RoundedCornerShape(12.dp)),
+                    elevation = CardDefaults.cardElevation( if (ageRange == "6-8") 8.dp else 2.dp),
+                    colors = CardDefaults.cardColors(Color.DarkGray)
+                ) {
+                    Text("6-8", modifier = Modifier.padding(8.dp), color = Color.White)
+                }
+
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clickable { ageRange = "8-10" }
+                        .border(width = if (ageRange == "8-10") 2.dp else 2.dp, color = if (ageRange == "8-10") Color.White else Color.Gray, shape = RoundedCornerShape(12.dp)),
+                    elevation = CardDefaults.cardElevation( if (ageRange == "8-10") 8.dp else 2.dp),
+                    colors = CardDefaults.cardColors(Color.DarkGray)
+                ) {
+                    Text("8-10", modifier = Modifier.padding(8.dp), color = Color.White)
+                }
+
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clickable { ageRange = "10-12" }
+                        .border(width = if (ageRange == "10-12") 2.dp else 2.dp, color = if (ageRange == "10-12") Color.White else Color.Gray, shape = RoundedCornerShape(12.dp)),
+                    elevation = CardDefaults.cardElevation( if (ageRange == "10-12") 8.dp else 2.dp),
+                    colors = CardDefaults.cardColors(Color.DarkGray)
+                ) {
+                    Text("10-12", modifier = Modifier.padding(8.dp), color = Color.White)
+                }
             }
 
-            Card(
-                modifier = Modifier
-                    .padding(4.dp)
-                    .clickable { ageRange = "10-12" },
-                elevation = CardDefaults.cardElevation( if (ageRange == "10-12") 8.dp else 2.dp)
-            ) {
-                Text("10-12", modifier = Modifier.padding(8.dp))
-            }
-        }
+            Spacer(modifier = Modifier.height(24.dp))
 
-        Button(onClick = {
-            CoroutineScope(Dispatchers.Main).launch {
-                viewModel.saveAvatar(name, gender, ageRange)
-                context.startActivity(Intent(context, MainActivity::class.java))
-                (context as Activity).finish()
+            OutlinedButton(
+                onClick = {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        viewModel.saveAvatar(name, gender, ageRange)
+                        context.startActivity(Intent(context, MainActivity::class.java))
+                        (context as Activity).finish()
+                    }
+                }) {
+                Text("Speichern")
             }
-        }) {
-            Text("Weiter")
         }
     }
 }
