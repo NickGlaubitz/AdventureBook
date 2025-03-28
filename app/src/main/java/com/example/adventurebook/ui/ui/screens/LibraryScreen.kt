d2c6eda982a73f612f0c49e305c8203fa0f512de
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-03-28 13:29:34 +0100

    LibraryScreen UI Update

--- app/src/main/java/com/example/adventurebook/ui/ui/screens/LibraryScreen.kt
+++ app/src/main/java/com/example/adventurebook/ui/ui/screens/LibraryScreen.kt
@@ -1,15 +1,19 @@
 package com.example.adventurebook.ui.ui.screens
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
@@ -18,7 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -37,23 +43,29 @@ fun LibraryScreen(
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 8.dp)
+                    .border(width = 2.dp, color = Color(0xFF6650a4), shape = RoundedCornerShape(12.dp))
+                    //.padding(bottom = 8.dp)
                     .clickable { navController.navigate("story/${story.id}") },
-                elevation = CardDefaults.cardElevation(4.dp)
+                elevation = CardDefaults.cardElevation(4.dp),
+                colors = CardDefaults.cardColors(Color.DarkGray)
             ) {
-                Row(modifier = Modifier.padding(16.dp)) {
+                Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
                     AsyncImage(
                         model = story.ImageUrl,
                         contentDescription = "Story Image",
-                        modifier = Modifier.size(50.dp),
+                        modifier = Modifier.size(70.dp),
                         contentScale = ContentScale.Crop
                     )
 
                     Spacer(modifier = Modifier.width(16.dp))
 
-                    Column {
-                        Text(story.title, style = MaterialTheme.typography.headlineMedium)
-                        Text(story.content.take(50) + "...", style = MaterialTheme.typography.bodyMedium)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(story.title, style = MaterialTheme.typography.headlineMedium, color = Color.LightGray)
+                        Text(story.content.take(50) + "...", style = MaterialTheme.typography.bodyLarge, color = Color.LightGray)
                     }
                 }
             }
