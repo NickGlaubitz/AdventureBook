28e6606818f4d764037ed1c3e8a859c9da6a29f8
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-05-05 08:48:31 +0200

    Small Ui Update

--- app/src/main/java/com/example/adventurebook/Constants.kt
+++ app/src/main/java/com/example/adventurebook/Constants.kt
@@ -6,5 +6,5 @@ import com.aallam.openai.api.image.internal.ImageResponseFormat.Companion.url
 
 
 object Constants{
-    const val OPENAI_API_KEY = "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"
+    const val OPENAI_API_KEY = ""
 }
