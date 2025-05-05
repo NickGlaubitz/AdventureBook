2b671c75f22481db1052a78dcd79e02da88fcee8
Domenique Glaubitz <nick.glaubitz@icloud.com>
2025-05-05 08:47:10 +0200

    Small Ui Update

--- app/src/main/java/com/example/adventurebook/Constants.kt
+++ app/src/main/java/com/example/adventurebook/Constants.kt
@@ -8,14 +8,3 @@ import com.aallam.openai.api.image.internal.ImageResponseFormat.Companion.url
 object Constants{
     const val OPENAI_API_KEY = "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"
 }
-
-
-// Animation für Bildübergang
-//Crossfade(targetState = story!!.ImageUrl) { url ->
-//    AsyncImage(
-//        model = url,
-//        contentDescription = "Story Image",
-//        modifier = Modifier.fillMaxSize(),
-//        contentScale = ContentScale.Crop
-//    )
-//}
